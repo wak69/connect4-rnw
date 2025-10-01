07aeeed4cebc5fc6b2cdf1225baeed69eac3df6a
wak69 <waelkaraki1234@gmail.com>
2025-10-01 19:36:52 +0300

    added place function

--- connect4.c
+++ connect4.c
@@ -21,8 +21,30 @@ void printBoard(char** board) {
     }
 }
 
+int place(char c, int col, char** board) {
+    if (col < 0 || col > 6) {
+        return 2;
+    }
+    if (board[0][col] != '.') {
+        return 1;
+    }
+    int i = 0;
+    while (i < 6 && board[i][col] == '.') {
+        i++;
+    }
+    board[i - 1][col] = c;
+    return 0;
+}
+
 int main() {
     char** board = initBoard();
+    int idk = place('A', 6, board);
+    int guh = place('B', 7, board);
+    int bruh;
+    for (int i = 0; i < 7; i++) {
+    bruh = place('A', 5, board);
+    }
+    printf("%d %d %d\n", idk, guh, bruh);
     printBoard(board);
     return 0;
 }
